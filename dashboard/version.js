e45dc1668eb1b8981ab2bed797d430b823f5a7ec
Aaronkhodami <aaron.a.khodami@gmail.com>
2026-05-20 11:52:59 +0300

    add version footer to login page, git hooks for auto versioning

--- dashboard/version.js
+++ dashboard/version.js
@@ -1,4 +1,4 @@
 // -- Interface version ------------------------------------------------------
 // This file is managed automatically by git post-commit hook.
 window.INTERFACE_VERSION_DATE = "2026-05-20";
-window.INTERFACE_VERSION_SHA  = "fc20191ff201333f85a1048e237a4648b622ade0";
+window.INTERFACE_VERSION_SHA  = "11a61bf0b6046e882599efa52eccbcbaa891560d";
