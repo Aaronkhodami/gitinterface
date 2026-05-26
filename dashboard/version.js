4239d6b386bae4dd926378003957c8efd297a552
Aaronkhodami <aaron.a.khodami@gmail.com>
2026-05-26 16:32:58 +0300

    update document viewer and sligth improvement

--- dashboard/version.js
+++ dashboard/version.js
@@ -1,4 +1,4 @@
 // -- Interface version ------------------------------------------------------
 // This file is managed automatically by git commit hook.
 window.INTERFACE_VERSION_DATE = "2026-05-25";
-window.INTERFACE_VERSION_SHA  = "5c7ea930da9cdc430d01e6acad1a7dae1032d391";
+window.INTERFACE_VERSION_SHA  = "58ba4a7e677419436bfacf2c67ae8e574a9e6e17";
